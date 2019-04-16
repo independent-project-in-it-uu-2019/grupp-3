006755f69e4d901a8a20550516e97990a64243f5
Elisabethryan <ebethryan@gmail.com>
2019-04-16 16:47:08 +0200

    removed button import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import headerImg from './images/header.jpg'
 import logo from './images/UU_LOGO.png'
 import Header from './components/Header'
-import Button from 'react-bootstrap/Button';
 import StartPage from './components/startPage';
 
 class App extends Component {

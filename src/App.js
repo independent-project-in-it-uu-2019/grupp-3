2f0e05555334011219ece5150ccc0781d9cfbdb6
Norah Naver <naver.norah@gmail.com>
2019-04-16 16:09:33 +0200

    Fixed design, hover and onClick function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import SmallInfoBox from './components/smallInfoBox'
-import EntryBackground from './images/EntryBackground.jpg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmallInfoBox EntryBackground={EntryBackground}/>
+        <SmallInfoBox
+          Title="Tool Title"
+          Text="At vero eos et accusamus et iusto odio dignissimos ducimus qu blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate nonprovident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."
+        />
       </div>
     );
   }
--- src/components/smallInfoBox.js
+++ src/components/smallInfoBox.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react'
 import '../css/smallInfoBox.css'
+import Plus from '../svg/plus.svg'
 
 export default class SmallInfoBox extends Component {
   constructor(props) {
     super(props);
   }
+  onItemClick(){
+    alert("Not implemented yet!");
+  }
 
   render() {
     return (
-    <div className="smallInfoBox">
-      <div class="card" style={{height: 150, width: 500}}>
-        <img src={this.props.EntryBackground} class="card-img" style={{height: 150, width: 500}}/>
-        <div class="card-img-overlay">
-          <h2>Tool Title</h2>
-          <div className="seperator"></div>
-          <p>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer </p>
-          <div className="plus">
-              <i class="fas fa-camera" style={{color: "black"}}/>
+    <div className="smallInfoBox" style={{height: 150, width: 500}}>
+          <div className="title">
+            <a onClick={this.onItemClick}>
+              <h2>{this.props.Title}</h2>
+            </a>
           </div>
-        </div>
-      </div>
+          <div className="seperator" style={{height: 150}}/>
+          <p>{this.props.Text}</p>
+          <div className="plus">
+            <a onClick={this.onItemClick}>
+              <span>
+                <img src={Plus} style={{height: 70, width: 70}}/>
+              </span>
+            </a>
+          </div>
     </div>
     )
   }
+
 }

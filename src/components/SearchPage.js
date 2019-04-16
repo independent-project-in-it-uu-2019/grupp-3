b2551af35b92ffb1e13874afa7f481702f70c362
tijanab <baractijana@gmail.com>
2019-04-16 15:29:55 +0200

    started with dropdown in search, functional but wrong position

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export class SearchPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+        }
+    }
+
+    openDropDown = () => {
+        this.setState({
+            isOpen: !this.state.isOpen,
+        })
+    };
+
     render() {
         return (
             <div>
-
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" onClick={() => this.openDropDown()}>
+                        Dropdown button
+                    </button>
+                    <div className={`dropdown-menu ${this.state.isOpen ? 'show' : ''}`} aria-labelledby="dropdownMenuButton">
+                        <a className="dropdown-item" href="#">Action</a>
+                        <a className="dropdown-item" href="#">Another action</a>
+                        <a className="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </div>
             </div>
         );
     }

e0dd7a1a79b7acde7ab25f13ad982cacd80b595b
Norah Naver <naver.norah@gmail.com>
2019-04-16 18:05:25 +0200

    deleted content in return in japp.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import SmallInfoBox from './components/smallInfoBox'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SmallInfoBox
-          Title="Tool Title"
-          Text="At vero eos et accusamus et iusto odio dignissimos ducimus qu blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate nonprovident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."
-        />
-      </div>
     );
   }
 }

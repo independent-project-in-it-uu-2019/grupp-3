bf7f73b5eec96e3ee8f9cf097ee85b8f7c8eacec
Norah Naver <naver.norah@gmail.com>
2019-04-17 18:39:49 +0200

    Fixed smallInfoBox so it works with master, also fixed so it is responsive

---
+++ src/components/infoBoxTech.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import '../css/infoBoxTech.css'
+
+export default class SmallInfoTech extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    <div className="smallInfoTech" style={{height: 150, width: 500}}>
+
+    </div>
+    )
+  }
+}

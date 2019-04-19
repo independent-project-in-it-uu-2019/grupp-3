759ddbcfe85320513f304cf337a9f9a032d12a67
Norah Naver <naver.norah@gmail.com>
2019-04-19 12:45:03 +0200

    added picture to infoboxTech

--- src/components/CircleImage.js
+++ src/components/CircleImage.js
@@ -5,14 +5,14 @@ export default class CircleImage extends Component {
 
   constructor(props) {
       super(props);
-  }  
+  }
 
   render() {
     return (
       <div className="CircleImage" style={{
-                                    height: this.props.height, 
-                                    width: this.props.width, 
-                                    borderRadius: 50+"%", 
+                                    height: this.props.height,
+                                    width: this.props.width,
+                                    borderRadius: 50+"%",
                                     backgroundImage: `url(${this.props.imageURL})`,
                                     backgroundSize: "cover",
                                     }}>

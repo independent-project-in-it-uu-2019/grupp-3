44c4432b72d8599eef88fa0a8b2280e9feca68b9
tijanab <baractijana@gmail.com>
2019-04-21 14:49:09 +0200

    has one working search field with different values

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,82 +1,55 @@
 import React, {Component} from 'react';
-import {Navbar} from 'react-bootstrap';
-import {Dropdown} from 'react-bootstrap';
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import FormControl from "react-bootstrap/es/FormControl";
+import {Dropdown} from 'semantic-ui-react'
 
-export class SearchPage extends Component {
 
-    render() {
-        return (
-            <div>
+const options1 = [
+    {key: 'angular', text: 'Angular', value: 'angular'},
+    {key: 'css', text: 'CSS', value: 'css'},
+    {key: 'design', text: 'Graphic Design', value: 'design'},
+    {key: 'ember', text: 'Ember', value: 'ember'},
+    {key: 'html', text: 'HTML', value: 'html'},
+    {key: 'ia', text: 'Information Architecture', value: 'ia'},
+    {key: 'javascript', text: 'Javascript', value: 'javascript'},
+    {key: 'mech', text: 'Mechanical Engineering', value: 'mech'},
+    {key: 'meteor', text: 'Meteor', value: 'meteor'},
+    {key: 'node', text: 'NodeJS', value: 'node'},
+    {key: 'plumbing', text: 'Plumbing', value: 'plumbing'},
+    {key: 'python', text: 'Python', value: 'python'},
+    {key: 'rails', text: 'Rails', value: 'rails'},
+    {key: 'react', text: 'React', value: 'react'},
+    {key: 'repair', text: 'Kitchen Repair', value: 'repair'},
+    {key: 'ruby', text: 'Ruby', value: 'ruby'},
+    {key: 'ui', text: 'UI Design', value: 'ui'},
+    {key: 'ux', text: 'User Experience', value: 'ux'},
+]
+const options2 = [
+    {key: 'angular', text: 'Angular', value: 'angular'},
+    {key: 'css', text: 'CSS', value: 'css'},
+    {key: 'design', text: 'Graphic Design', value: 'design'},
+    {key: 'ember', text: 'Ember', value: 'ember'},
+    {key: 'html', text: 'HTML', value: 'html'},
+    {key: 'ia', text: 'Information Architecture', value: 'ia'},
+    {key: 'javascript', text: 'Javascript', value: 'javascript'},
+    {key: 'mech', text: 'Mechanical Engineering', value: 'mech'},
+    {key: 'meteor', text: 'Meteor', value: 'meteor'},
+    {key: 'node', text: 'NodeJS', value: 'node'},
+    {key: 'plumbing', text: 'Plumbing', value: 'plumbing'},
+    {key: 'python', text: 'Python', value: 'python'},
+    {key: 'rails', text: 'Rails', value: 'rails'},
+    {key: 'react', text: 'React', value: 'react'},
+    {key: 'repair', text: 'Kitchen Repair', value: 'repair'},
+    {key: 'ruby', text: 'Ruby', value: 'ruby'},
+    {key: 'ui', text: 'UI Design', value: 'ui'},
+    {key: 'ux', text: 'User Experience', value: 'ux'},
+]
 
-                <Navbar className ="justify-content-center">
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Cost
-                        </Dropdown.Toggle>
+const DropdownExampleMultipleSelection = () => {
+    return (
+        <Dropdown placeholder='Cost' fluid multiple selection options={options1}>
+        </Dropdown>,
+            <Dropdown placeholder='Platform' fluid multiple selection options={options2}>
+            </Dropdown>
+    )
+};
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Platform
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Hardware
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                           Misc
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Education
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Navbar>
-                <Navbar className="justify-content-center">
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                </Navbar>
-            </div>
-        );
-    }
-}
-
-export default SearchPage;
+export default DropdownExampleMultipleSelection

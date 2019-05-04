e4a680b18c37afc91a4967e95f9b9ea4f716b232
tijanab <baractijana@gmail.com>
2019-05-04 17:08:22 +0200

    better dropdown with bootstrap

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,50 +1,45 @@
 import React, { Component } from 'react'
-import { Dropdown, Grid } from 'semantic-ui-react'
+import Select from 'react-select';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '../css/searchPage.css'
 
-class DropdownExampleRemote extends Component {
-    componentWillMount() {
-        this.setState({
-            options: [
-                { key: 1, text: 'Nummer 1', value: 'Nummer 1' },
-                { key: 2, text: 'Nummer 2', value: 'Nummer 2' },
-                { key: 3, text: 'Nummer 3', value: 'Nummer 3' },
-            ],
-            options2: [
-                { key: 1, text: 'SUNDAY', value: 'SUNDAY' },
-                { key: 2, text: 'MONDAY', value: 'MONDAY' },
-                { key: 3, text: 'TUESDAY', value: 'TUESDAY' },
-            ],
-            value: '',
-            member: '',
-            hour: '',
-        })
-    }
+const techCompanies = [
+    { label: "Apple", value: 1 },
+    { label: "Facebook", value: 2 },
+    { label: "Netflix", value: 3 },
+    { label: "Tesla", value: 4 },
+    { label: "Amazon", value: 5 },
+    { label: "Alphabet", value: 6 },
+];
 
-    render() {
-        const {options, options2, value } = this.state
+const Search = () => (
+    <div className="entireSearch">
+        <div className="container" className="dropdown">
+            <div className="row">
+                <div className="col-sm">
+                    <Select options={techCompanies}
+                        isMulti />
+                </div>
+                <div className="col-sm">
+                    <Select options={techCompanies}
+                        isMulti />
+                </div>
+                <div className="col-sm">
+                    <Select options={techCompanies}
+                        isMulti />
+                </div>
+                <div className="col-sm">
+                    <Select options={techCompanies}
+                        isMulti />
+                </div>
+                <div className="col-sm">
+                    <Select options={techCompanies}
+                        isMulti />
+                </div>
+            </div>
+        </div>
+    </div>
+);
 
-        return (
-            <Grid>
-                <Grid.Column width={2}>
-                    <Dropdown placeholder='Cost' className={"ownDropdown"} fluid multiple selection options={options} />
-                </Grid.Column>
-                <Grid.Column width={2}>
-                    <Dropdown placeholder='Platform' className={"ownDropdown"} fluid multiple selection options={options} />
-                </Grid.Column>
-                <Grid.Column width={2}>
-                    <Dropdown placeholder='Hardware' className={"ownDropdown"} fluid multiple selection options={options} />
-                </Grid.Column>
-                <Grid.Column width={2}>
-                    <Dropdown placeholder='Misc' className={"ownDropdown"} fluid multiple selection options={options} />
-                </Grid.Column>
-                <Grid.Column width={2}>
-                    <Dropdown placeholder='Education' className={"ownDropdown"} fluid multiple selection options={options} />
-                </Grid.Column>
-            </Grid>
-        )
-    }
-}
-
-export default DropdownExampleRemote
+export default Search

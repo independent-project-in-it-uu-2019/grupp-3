490c85d0dc0c5e62bbade5bce60edcc69e27381f
Elisabethryan <ebethryan@gmail.com>
2019-05-06 15:02:20 +0200

    moved and margins changed

--- src/components/StartPageInfo.js
+++ src/components/StartPageInfo.js
@@ -13,8 +13,9 @@ export class StartPageInfo extends Component {
 
     renderImageRight() {
         return (
+            <div classname="imgright">
             <div className="row justify-content-center align-items-center">
-                <div className="col-md-7">
+                <div className="col-md-5">
                     <div className="col-md-6 d-md-none" align="center">
                         <CircleImage width={"50vw"} height={"50vw"} imageURL={require('../images/ananas.jpg')}/>
                     </div>
@@ -30,6 +31,7 @@ export class StartPageInfo extends Component {
                     </div>
                 </div>
             </div>
+            </div>
         )
     }
 
@@ -41,13 +43,13 @@ export class StartPageInfo extends Component {
                         <CircleImage width={"14vw"} height={"14vw"} imageURL={require('../images/ananas.jpg')}/>
                     </div>
                 </div>
-                <div className="col-md-7">
+                <div className="col-md-5">
                     <div className="col-md-6 d-md-none" align="center">
                         <CircleImage width={"50vw"} height={"50vw"} imageURL={require('../images/ananas.jpg')}/>
                     </div>
                     <h1>Headline</h1>
                     <p className="bodyText">N.E.F.T is your help to improve your lectures and classes by providing
-                        tools to digitalize your content. It will make your classes more
+                        tools to digitalize jjjjsses more
                         available to students and let them consume the material in their own.
                         phase.</p>
                 </div>

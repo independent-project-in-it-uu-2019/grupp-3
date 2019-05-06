c1a0838014e5aac825743bb23d32b491377c263f
Elisabethryan <ebethryan@gmail.com>
2019-05-06 22:10:01 +0200

    finished page!

--- src/components/StartPageButtons.js
+++ src/components/StartPageButtons.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import '../css/StartPageButtons.css';
 import '../css/colors.css'
+import {BrowserRouter as Router, Link} from "react-router-dom";
 
 export class StartPageButtons extends Component {
     render() {
         return (
             <div className="col-md-12 buttonsdiv" role="group" aria-label="ButtonsFrontpage" align="center">
-                <button type="button" className="startbutton">Search</button>
-                <button type="button" className="startbutton">Browse</button>
+                <Link to={"/search"}>
+                    <button type="button" className="startbutton"> Search </button>
+                </Link>
+                <Link to={"/browse"}>
+                    <button type="button" className="startbutton"> Browse </button>
+                </Link>
             </div>
         );
     }
--- src/components/StartPageInfo.js
+++ src/components/StartPageInfo.js
@@ -17,17 +17,16 @@ export class StartPageInfo extends Component {
             <div className="row justify-content-center align-items-center">
                 <div className="col-md-5">
                     <div className="col-md-6 d-md-none" align="center">
-                        <CircleImage width={"50vw"} height={"50vw"} imageURL={require('../images/ananas.jpg')}/>
+                        <CircleImage width={"50vw"} height={"50vw"} imageURL={require('../images/pic2.jpg')}/>
                     </div>
-                    <h1>Headline</h1>
-                    <p className="bodyText">N.E.F.T is your help to improve your lectures and classes by providing
-                        tools to digitalize your content. It will make your classes more
-                        available to students and let them consume the material in their own.
-                        phase.</p>
+                    <h1>Learning should be fun</h1>
+                    <p className="bodyText">By exploiting the positive feedback we usually get from tapping
+                        around with our phone or computer in education, learning does not
+                        only get more effective. It also makes it more fun!</p>
                 </div>
                 <div className="col-md-2">
                     <div className="imgContainer d-none d-md-block float-left">
-                        <CircleImage width={"14vw"} height={"14vw"} imageURL={require('../images/ananas.jpg')}/>
+                        <CircleImage width={"14vw"} height={"14vw"} imageURL={require('../images/pic2.jpg')}/>
                     </div>
                 </div>
             </div>
@@ -40,17 +39,17 @@ export class StartPageInfo extends Component {
             <div className="row justify-content-center align-items-center">
                 <div className="col-md-2">
                     <div className="imgContainer d-none d-md-block float-center">
-                        <CircleImage width={"14vw"} height={"14vw"} imageURL={require('../images/ananas.jpg')}/>
+                        <CircleImage width={"14vw"} height={"14vw"} imageURL={require('../images/pic1.jpg')}/>
                     </div>
                 </div>
                 <div className="col-md-5">
                     <div className="col-md-6 d-md-none" align="center">
-                        <CircleImage width={"50vw"} height={"50vw"} imageURL={require('../images/ananas.jpg')}/>
+                        <CircleImage width={"50vw"} height={"50vw"} imageURL={require('../images/pic1.jpg')}/>
                     </div>
-                    <h1>Headline</h1>
+                    <h1>What is N.E.F.T?</h1>
                     <p className="bodyText">N.E.F.T is your help to improve your lectures and classes by providing
-                        tools to digitalize jjjjsses more
-                        available to students and let them consume the material in their own.
+                        tools to digitalize your content. It will make your classes more
+                        available to students and let them consume the material in their own
                         phase.</p>
                 </div>
             </div>

600d6da9a5c045f42bcc5f007972e45aa6872f61
Norah Naver <naver.norah@gmail.com>
2019-06-02 11:31:01 +0200

    changed the about text

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default class About extends Component {
   constructor(props) {
       super(props);
   }
-  
+
   goBack() {
       window.history.back();
   }
@@ -24,12 +24,15 @@ export default class About extends Component {
         <div className="aboutText">
           <p>There are many different methods and tools to make education more diverse.
           Finding information about these methods and tools is not always easy.
-          To help teachers in their education, this project is about creating a website that functions as an online catalog of tools and methods.
+          To help teachers in their education, the purpose of this project is creating
+          a website that functions as an online catalog of tools and methods.
           </p>
-          <p>The project is done by two different groups on the Master Programme in Computer and Information Engineering at Uppsala University.
-          Building this website has been the two groups’ bachelor thesis.
-          The first group, which work at the project the Spring in 2018, developed a database in which they stored different tools and methods.
-          The second group continued the work in the Spring 2019 and developed an user friendly interface to the website.
+          <p>The project is done by two different groups on the Master Programme in Computer
+          and Information Engineering at Uppsala University. Building this website has been
+          the work behind two groups’ bachelor thesis. The first group, who worked on the
+          project in the Spring of 2018, developed a database in which they stored different
+          tools and methods. The second group continued the work in the Spring of 2019 and
+          developed an user friendly interface for the website. 
           </p>
         </div>
       </div>
